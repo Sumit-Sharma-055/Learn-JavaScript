61a6c369561552b5dd833f2c1aa7294fa27b9a51
Sumit Sharma <xumit97575235@gmail.com>
2025-05-11 17:55:24 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+console.log("JAI SHREE RAM");
+const accountId = 122334 // conts is permanant 
+let accountName = "Sumit Sharma"
+var accountCity = "Mathura" // please please do not use the var keyword in future 
+let accountEmail = "sumit@gmail.com"
+accountState = "U.P"
+var accountpassword = "121212"
+let AccountNick
+
+
+/*
+if we change the conts keyword it will show the error 
+ 
+*/
+//////////////////////////////////////////////
+/*
+prefer not to use var keyword
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountName, accountEmail, accountpassword, accountCity, accountState, AccountNick])
