793f9bc691ab5456346d0c61b330eaa10b10d40b
Sumit Sharma <xumit97575235@gmail.com>
2025-05-12 11:29:37 +0000

    comperitaion in js

---
+++ 01_basics/04_comperesion.js
@@ -0,0 +1,19 @@
+console.log("JAI SHREE RAM");
+// comperision in js
+
+// console.log(2 > 3);
+// console.log(2 < 3);
+// console.log(2 <= 3);
+// console.log(2 <= 3);
+// console.log(2 == 3);
+// console.log(2 === 2);
+
+// console.log("2" > 2)
+// console.log("2" == 2)
+// console.log("23" == true)
+// console.log("2" == "2")
+// console.log(0 == null)
+// console.log(null == undefined)
+
+console.log("2" === 2);
+console.log("2" === '2');
