2cdf8d8f7e2293323d9b64fafd11886dce2fa2fa
Sumit Sharma <xumit97575235@gmail.com>
2025-05-12 10:11:24 +0000

    operators in javaScript

--- 01_basics/03_typeconversion_operation.js
+++ 01_basics/03_typeconversion_operation.js
@@ -2,22 +2,22 @@ console.log("JAI SHREE RAM");
 
 let score = 23
 
-console.log(typeof score);
+// console.log(typeof score);
 // console.log(typeof(score));
 
 let scoreNumber = Number(score)
-console.log(typeof scoreNumber);
-console.log(scoreNumber);
+// console.log(typeof scoreNumber);
+// console.log(scoreNumber);
 
 // 33 => 33
 // "33ab" => NaN
 // true  => 1 , false => 0
 
 let isBoolean = ""
-console.log(isBoolean);
+// console.log(isBoolean);
 
 let areBoolean = Boolean(isBoolean)
-console.log(areBoolean);
+// console.log(areBoolean);
 
 // 33 => true
 // 1 => true , 0 => false
@@ -27,3 +27,32 @@ console.log(areBoolean);
 // syntax
 // let variableName = ChangingTheVariableDoYouWant (variable)
 
+// ******************************operators***********************************
+let number = 12
+let negNumber = -number
+// console.log(negNumber);
+
+// operations
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 2);
+// console.log(2 % 2);
+
+let str1 = "sumit"
+let str2 = " sharma"
+// console.log(str1 + str2)
+// console.log(2 + 3)
+// console.log(2 + "3")
+// console.log("2" + 3)
+// console.log(2 + 3 + "5")
+// console.log("2" + 3 + 5)
+
+// console.log(true);
+
+let num1 = num2 = num3 = 2 + 3
+// console.log(num2);
+
+// increament operator
