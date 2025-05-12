18157ec4c8ddd482ff59774604a2113247974c5c
Sumit Sharma <xumit97575235@gmail.com>
2025-05-12 09:44:22 +0000

    type conversion

---
+++ 01_basics/03_typeconversion_operation.js
@@ -0,0 +1,29 @@
+console.log("JAI SHREE RAM");
+
+let score = 23
+
+console.log(typeof score);
+// console.log(typeof(score));
+
+let scoreNumber = Number(score)
+console.log(typeof scoreNumber);
+console.log(scoreNumber);
+
+// 33 => 33
+// "33ab" => NaN
+// true  => 1 , false => 0
+
+let isBoolean = ""
+console.log(isBoolean);
+
+let areBoolean = Boolean(isBoolean)
+console.log(areBoolean);
+
+// 33 => true
+// 1 => true , 0 => false
+// "" => false
+
+// we can change the datatypes of any types in js with the help of the class 
+// syntax
+// let variableName = ChangingTheVariableDoYouWant (variable)
+
