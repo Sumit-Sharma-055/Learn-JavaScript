6fa735a74c03ab389760e38201a40b1db1ac9344
Sumit Sharma <xumit97575235@gmail.com>
2025-05-12 09:02:04 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+console.log("JAI SHREE RAM")
+
+"use strict";
+
+
+// alert("JAI SHREE RAM") is use only the web browser
+let number = 12
+let string = 'sumit'
+let age = 19
+let isLogin = true
+
+console.table([number, string, age, isLogin])
+/*
+
+
+number 
+bigint 
+string
+boolean
+null
+undefined
+symbol
+
+
+object
+
+*/
+
+console.log(typeof null)
+console.log(typeof undefined)
+    
