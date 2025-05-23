ba6abdf30a8dd36921236387210f4b83eb4b9f3c
Sumit Sharma <xumit97575235@gmail.com>
2025-05-23 17:45:16 +0000

    nnm

--- 01_basics/07_dateAndTime.js
+++ 01_basics/07_dateAndTime.js
@@ -1,1 +1,17 @@
 console.log("JAI SHREE RAM");
+let myDate = new Date();
+// console.log(myDate.getFullYear());
+// console.log(myDate.getTime());
+// console.log(myDate.toString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.getSeconds());
+// console.log(myDate.toDateString());
+// console.log(myDate());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+// console.log(myDate.getDay());
+// const a = typeOf(myDate);
+
+let currentDate = new Date(2023-02-23);
+console.log(currentDate);
