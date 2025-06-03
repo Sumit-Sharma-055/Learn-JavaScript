66ec4dd43c1f21891ba711ea50b5a49d1513f375
Sumit Sharma <xumit97575235@gmail.com>
2025-06-03 17:12:09 +0000

    function

---
+++ 03_basics/01_function_and_scope.js
@@ -0,0 +1,54 @@
+console.log("Jai shree ram ");
+
+// function
+
+/* function name() { 
+
+}
+
+name()
+*/
+
+// function add(a, b) {
+//     console.log(a+b);
+    // return 
+    
+// }
+
+// add(4,6)
+
+// rest operator
+// function loginuser (...num1) {
+//     return num1
+// }
+// console.log(loginuser(200, 500, 65, 888))
+
+// function loginuser1 (val1, val2, ...num1) {
+//     return val1, val2, num1
+// }
+// console.log(loginuser1(200, 500, 65, 888))
+
+// function loginuser2 (...num1) {
+//     console.log(num1 + num1);
+    
+// }
+
+// console.log(loginuser2(200, 500, 65, 888))
+
+// const mynewarray = [1080, 200, 300]
+// function myfun (mynewarray) {
+//     return mynewarray[1]
+// }
+// myfun(mynewarray)
+// // myfun()
+
+let newArray = [100, 200, 300]; //Add commentMore actions
+
+function secondValueOfArray(array) {
+    return array[2];
+}
+
+console.log(secondValueOfArray(newArray));
+
+
+
