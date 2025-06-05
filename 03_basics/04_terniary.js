be0d92bd841e5fdd106e9bc479d77870c303a426
Sumit Sharma <xumit97575235@gmail.com>
2025-06-05 09:41:15 +0000

    terniary oprator

---
+++ 03_basics/04_terniary.js
@@ -0,0 +1,15 @@
+console.log("JAI SHRREE RAM");
+
+// terniary operator 
+
+//syntax of the terniary operator 
+
+// condition ? expressionIfTrue : expressionIfFalse
+
+const age = 20;
+const status = age >= 18 ? "Adult" : "Minor";
+console.log(status); // Output: Adult
+
+let marks = 54;
+const result = marks >= 33 ? "pass" : "fail";
+console.log(result);
