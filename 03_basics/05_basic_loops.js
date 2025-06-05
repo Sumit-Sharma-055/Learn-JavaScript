81ee5e6e8da1160a27810bd68791f73b6999e041
Sumit Sharma <xumit97575235@gmail.com>
2025-06-05 10:51:47 +0000

    basics loops in js

---
+++ 03_basics/05_basic_loops.js
@@ -0,0 +1,33 @@
+console.log("JAI SHREE RAM");
+
+//loops / iteration
+//\\ 1. for loop
+
+for (let i = 0; i < 10; i++) {
+    // console.log(i);  
+}
+
+
+let num = 2 
+for (let index = 1; index <= 10; index++) {
+    console.log(index*num);
+    
+    
+}
+console.log("-------------------");
+
+//\\ 2. while loop
+let i = 0;
+while (i<=10) {
+    console.log(i);
+    i++;
+}
+console.log("----------");
+
+//\\ 3. do-while loop
+
+let a = 0;
+do {
+    console.log(a);
+    a++;
+} while (a<=10);
