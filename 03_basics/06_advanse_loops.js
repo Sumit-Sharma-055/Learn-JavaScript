7afdb86faa7b6027b5a0911788234c60c573252b
Sumit Sharma <xumit97575235@gmail.com>
2025-06-05 12:07:32 +0000

    loops off inn

---
+++ 03_basics/06_advanse_loops.js
@@ -0,0 +1,49 @@
+console.log("JAI SHREE RAM");
+
+// some others loops in java Script
+// for of loop
+
+const cars = ["BMW", "Volvo", "Mini"];
+
+let text = 1;
+for (let xtext of cars) {
+  text += x;
+}
+
+// for in loop
+// for each loop
+// in array
+
+// const coding = ["sumit", "mant", 18];
+// coding.forEach( (val) => {
+//     console.log(val);
+    
+// });
+
+// // in object
+
+// const myobj = [
+//     {
+//         name : "sumit",
+//         age : 18
+//     },
+//     {
+//         name : "shyam",
+//         age : 13
+//     },
+//     {
+//         name : "kapil",
+//         age : 34
+//     }
+// ];
+
+// myobj.forEach((item) => {
+//     console.log(item);
+    
+// })
+
+
+
+// filter method 
+// reduce method
+
